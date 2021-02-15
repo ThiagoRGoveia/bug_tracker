0eee514ce676768f6a34d28feecbea506330f23f
Thiago <t.r.goveia@gmail.com>
2021-02-15 09:11:47 -0300

    Adds test database

--- config/database.php
+++ config/database.php
@@ -95,7 +95,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
+            'database' => env('DB_DATABASE_TEST', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
