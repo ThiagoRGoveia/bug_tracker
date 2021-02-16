ca6f4852bdfacde21abe573658dccedbaba00f9c
Thiago <t.r.goveia@gmail.com>
2021-02-16 12:38:51 -0300

    Adds UserEloquentAdapter unit test for user login module

---
+++ tests/Adapter/Module/User/Login/UserEloquentAdapterTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Adapter\Module\User\Login;
+
+use Tests\TestCase;
+use App\Models\User as UserDb;
+use Tracker\Domain\Module\User\Login\Port\UserPort;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tracker\Adapter\Module\User\Login\UserEloquentAdapter;
+
+
+class UserEloquentAdapterTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testAdapterImplementsPort(): void
+    {
+        $adapter = new UserEloquentAdapter();
+        self::assertInstanceOf(UserPort::class, $adapter);
+    }
+
+    public function testFindUserByEmail()
+    {
+        $adapter = new UserEloquentAdapter();
+        $userModel = UserDb::factory()->create();
+
+        $user = $adapter->findUserByEmail($userModel->email);
+
+        $this->assertSame(
+            $user->id,
+            $userModel->id
+        );
+    }
+}
