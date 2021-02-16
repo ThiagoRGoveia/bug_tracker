332c31a4afbf3481e4f6d3db52797ccd976f56ae
Thiago <t.r.goveia@gmail.com>
2021-02-16 16:36:10 -0300

    Fixes typo and provides accesstoken as string to User class

--- adapter/Module/User/Login/AuthIlluminateAdapter.php
+++ adapter/Module/User/Login/AuthIlluminateAdapter.php
@@ -3,6 +3,7 @@
 namespace Tracker\Adapter\Module\User\Login;
 
 use Illuminate\Support\Facades\Auth;
+use Tracker\Domain\Module\User\Login\Entity\User;
 use Tracker\Domain\Module\User\Login\Port\AuthPort;
 use Tracker\Domain\Module\User\Login\Port\UserPort;
 use Tracker\Domain\Module\User\Login\Entity\AuthenicatedUser;
@@ -11,11 +12,11 @@ use Tracker\Domain\Module\User\Login\Entity\UnauthenticatedUser;
 class AuthIlluminateAdapter implements AuthPort
 {
     private $userPort;
-    public function __construct(UserPort $userport)
+    public function __construct(UserPort $userPort)
     {
-        $this->userport = $userport;
+        $this->userPort = $userPort;
     }
-    public function authenticate(string $email, string $password)
+    public function authenticate(string $email, string $password): User
     {
         if (!Auth::attempt(['email' => $email, 'password' => $password])) {
             return new UnauthenticatedUser();
@@ -24,7 +25,7 @@ class AuthIlluminateAdapter implements AuthPort
         Auth::login($user);
         return new AuthenicatedUser(
             $user->id,
-            $user->createToken('Personal Access Token')
+            $user->createToken('Personal Access Token')->accessToken
         );
     }
 }
